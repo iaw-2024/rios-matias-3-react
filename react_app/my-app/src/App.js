e3e2b779600cb377eb20369e60cc440aed411d52
rios-matias <rios.matias998@gmail.com>
2024-04-12 23:48:43 -0300

    feat: main program and nav bar added

---
+++ react_app/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componentes/Navbar';
+import React from 'react'
+import { Fragment } from 'react';
+function App() {
+  return (
+    <Fragment>
+      <Navbar/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ react_app/my-app/src/componentes/Navbar.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/main.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>BookReviews</h3>
+			<nav ref={navRef}>
+				<a href="/#">Listado</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
